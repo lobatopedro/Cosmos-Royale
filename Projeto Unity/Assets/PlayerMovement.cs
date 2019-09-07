b384d1be5ef316187ee9f75062ef19d1f363b2bd
Rodrigo Augusto Gimenes de Oliveira <rodrigo.augusto.gimenes@gmail.com>
2019-09-07 15:24:36 -0300

    Alterações

---
+++ Projeto Unity/Assets/PlayerMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Joystick joystick;
+    private Rigidbody spaceCraft;
+
+    public int velocity = 50;
+    public float maxSpeed = 50;
+
+    void Start()
+    {
+        joystick = FindObjectOfType<Joystick>();
+        spaceCraft = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        thrust(joystick.Horizontal, joystick.Vertical);
+        clampSpeed();
+    }
+
+    private void clampSpeed()
+    {
+        //Esse método limita a velocidade da nave de acordo com o valor colocado em maxSpeed.
+        float x = Mathf.Clamp(spaceCraft.velocity.x, -maxSpeed, maxSpeed);
+        float y = Mathf.Clamp(spaceCraft.velocity.y, -maxSpeed, maxSpeed);
+
+        spaceCraft.velocity = new Vector3(x, y);
+    }
+
+    private void thrust(float joystickX, float joystickY){
+
+        Vector3 forceX = transform.right * joystickX;
+        Vector3 forceY = transform.up * joystickY;
+        
+        spaceCraft.AddForce(forceY * velocity);
+        spaceCraft.AddForce(-forceX * velocity);
+    }
+
+
+
+}
