c7b85217f08ae762caa442f81bdd3ceb6eafef27
Rodrigo Augusto Gimenes de Oliveira <rodrigo.augusto.gimenes@gmail.com>
2019-11-02 19:23:28 -0300

    Collision

---
+++ Projeto Unity/Assets/CloseDeath.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseDeath : MonoBehaviour
+{
+
+
+    void Start()
+    {
+
+    }
+    void OnTriggerExit(Collider other)
+    {
+
+    }
+}
---
+++ Projeto Unity/Assets/Scripts/OnCollision.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OnCollision : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
