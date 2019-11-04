bc844cd31bdcd1c87a85b476e8ad43e35b186fce
GuilhermeFelizardo <guilherme_f.reis@hotmail.com>
2019-11-03 23:09:41 -0300

    Arrumando Bug Kozuki
    Arrumando Bug Kozuki

--- Projeto Unity/Assets/Scripts/PowerUp.cs
+++ Projeto Unity/Assets/Scripts/PowerUp.cs
@@ -7,35 +7,33 @@ public class PowerUp : MonoBehaviour
     
     public float multiplier = 1.7f;
     public float duration = 4f;
-    public GameObject pickupEffect;
     private bool onEffect = true;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            StartCoroutine( Pickup(other) );
+            transform.position = new Vector3(transform.position.x, transform.position.y, -300);
+            StartCoroutine(Pickup(other));
+            //transform.Find("Curve").GetComponent<MeshRenderer>().enabled = false;
+            //transform.Find("Cylinder").GetComponent<MeshRenderer>().enabled = false;
+            //transform.Find("Curve").GetComponent<Collider>().enabled = false;
+            //transform.Find("Cylinder").GetComponent<Collider>().enabled = false;
+            //transform.GetComponent<MeshRenderer>().enabled = false;
+            
         }
     }
 
     IEnumerator Pickup(Collider player)
     {
-        
-        Instantiate(pickupEffect, transform.position, transform.rotation);
-
-        player.transform.localScale *= multiplier;
-
-        transform.Find("Curve").GetComponent<MeshRenderer>().enabled = false;
-        transform.Find("Cylinder").GetComponent<MeshRenderer>().enabled = false;
-        transform.Find("Curve").GetComponent<Collider>().enabled = false;
-        transform.Find("Cylinder").GetComponent<Collider>().enabled = false;
-        transform.GetComponent<MeshRenderer>().enabled = false;
-        transform.GetComponent<Collider>().enabled = false;
+       
+        player.transform.localScale *= multiplier;    
+       
 
         yield return new WaitForSecondsRealtime(duration);
       
         player.transform.localScale /= multiplier;
-            
         Destroy(gameObject);
+
     }
 }
